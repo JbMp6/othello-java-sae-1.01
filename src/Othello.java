babc5c4eff26640fb23bb40faaa211d08decfc1e
JbMp6 <darkxyann@gmail.com>
2025-10-09 11:55:25 +0200

    Avancement sur le posisionement des cases

--- src/Othello.java
+++ src/Othello.java
@@ -6,8 +6,9 @@
 
 class Othello{
 	void principal(){
-		int[][] tab = tabJeu(16);
+		int[][] tab = tabJeu(8);
 		demarreJeu(tab);
+		
 	}
 	
 	/**
@@ -20,7 +21,60 @@ class Othello{
 		tab[(tab.length)/2][(tab.length-1)/2] = 2;
 		tab[(tab.length)/2][(tab.length)/2] = 1;
 		
-		afficheTabJeu(tab);
+		int touEnCours = 1;
+		
+		for (int i = 0; i < 9; i++){
+			System.out.println("\t------------------------------------");
+			afficheTabJeu(tab);
+			
+			int[][] caseA = caseAdverse(touEnCours, tab);
+			
+			if (touEnCours == 1){
+				touEnCours = 2;
+			}else{
+				touEnCours = 1;
+			}
+		}
+	}
+	
+	/**
+	 * Case possible a jouer pour un tour pour x ou o
+	 * @param tour : tour du joueur
+	 * @param tab : la table de jeu
+	 * @return int[][] matrice des x,y de pions adverse
+	 */
+	int[][] caseAdverse(int tour, int[][] tab) {
+		
+		int compt = 0;
+		int adverse;
+		
+		if ( tour == 1 ){
+			adverse = 2;
+		}else{
+			adverse = 1;
+		}
+		
+		for ( int i = 0; i < tab.length; i++){
+			for ( int y = 0; y < tab[i].length; y++){
+				if ( tab[i][y] == adverse ){
+					compt++;
+				}
+			}
+		}
+		
+		int[][] tabAdverse = new int[compt][2];
+		
+		compt = 0;
+		
+		for ( int i = 0; i < tab.length; i++){
+			for ( int y = 0; y < tab[i].length; y++){
+				if ( tab[i][y] == adverse ){
+					tabAdverse[compt] = new int[]{i,y};
+					compt++;
+				}
+			}
+		}
+		return tabAdverse;
 	}
 	
 	/**
@@ -42,6 +96,58 @@ class Othello{
 		tabVoisin[7] = new int[]{x-1, y-1};
 		
 		return tabVoisin;
+	}
+	
+	/**
+	 * Liste les voisins de plusieurs cases
+	 * @param tabAdverse : liste des position des pions adverse
+	 * @return int[][] matrice de tout les x,y des cases voisines
+	 */
+	int[][] listeCaseVoisin(int[][] tabAdverse) {
+		
+		int compt = 0;
+		
+		for ( int i = 0; i < tabAdverse.length; i++){
+			 
+			 int[][] rep = caseVoisin(tabAdverse[i][0], tabAdverse[i][1]);
+			 
+			 compt = compt + rep.length;
+		}
+		
+		int[][] listeVoisin = new int[compt][2];
+		//arrete ici
+		
+		return listeVoisin;
+	}
+	
+	/**
+	 * Verifi si les voisin d'une case sont des case jouable
+	 * @param tabVoisin : liste des voisin d'une case
+	 * @param tabJeu : table de jeu
+	 * @return int[][] matrice des x,y des cases jouable
+	 */
+	int[][] verifiCaseVoisin(int[][] tabVoisin, int[][] tabJeu) {
+		
+		int compt = 0;
+		
+		for ( int i = 0; i < tabVoisin.length; i++){
+			if ( tabJeu[tabVoisin[i][0]][tabVoisin[i][1]] == 0 ){
+				compt++;
+			}
+		}
+		
+		int[][] caseJouable = new int[compt][2];
+		
+		compt = 0;
+		
+		for ( int i = 0; i < tabVoisin.length; i++){
+			if ( tabJeu[tabVoisin[i][0]][tabVoisin[i][1]] == 0 ){
+				caseJouable[compt] = new int[]{tabVoisin[i][0],tabVoisin[i][1]};
+				compt++;
+			}
+		}
+				
+		return caseJouable;
 	}
 
 	
@@ -79,7 +185,7 @@ class Othello{
 		
 		for (int i = 0; i < tab.length; i++){
 			if ( i < 9){
-				System.out.print("\t" + (i+1) +" ");
+				System.out.print("\t" + (i+1) +"  ");
 			}else{
 				System.out.print("\t" + (i+1) +" ");
 			}
@@ -95,11 +201,7 @@ class Othello{
 					System.out.print("|x ");
 					
 			}
-<<<<<<< HEAD
-					
-=======
-				
->>>>>>> f28e409752ba7e6e062d5a4e1f62063cc0276fd3
+
 				if ( y == tab[y].length-1 ){
 					System.out.print("|\n");
 				}
@@ -123,10 +225,12 @@ class Othello{
 		while(true) {
 			int reponseLigne = SimpleInput.getInt("La ligne que vous voulez joué : ");
 			int reponseCase = SimpleInput.getInt("La case que vous voulez joué : ");
+			
 			int[] reponse = {reponseLigne, reponseCase};
 			
 			for (int i=0; i<casePossibleAJouer.length; i++) {
-				if (casePossibleAJouer[i] == reponse) {
+				if ((casePossibleAJouer[i][0] == reponse[0]) && (casePossibleAJouer[i][1] == reponse[1])) {
+					
 					return reponse;
 				}
 			}
